db424c125ee5f2fa1a489b48a71f261a4442bb89
AmruthaJoseph <124858409+AmruthaJoseph@users.noreply.github.com>
2023-02-09 10:23:56 +0530

    Add files via upload

---
+++ SampleJs/Sample.js
@@ -0,0 +1,14 @@
+let id = 10;
+console.log("id",id);
+
+let array = [10,30,20];
+console.log("Obj",array);
+
+let emp = {name:"Amrutha",Sal:2000}
+console.log("Employee",emp);
+
+function test1(){
+    console.log("Inside test1");
+}
+test1.apply();
+
