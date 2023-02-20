2d64df8744723b1c32508243314a4fd0ad1731c2
AmruthaJoseph <124858409+AmruthaJoseph@users.noreply.github.com>
2023-02-20 19:19:54 +0530

    Add files via upload

---
+++ React_Registration/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ParentComponent from "./components/function-comp/ParentComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
