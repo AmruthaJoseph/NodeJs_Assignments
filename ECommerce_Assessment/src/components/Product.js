bfacd9e01a51eced25b64f6a3a5d3c1caaf29fc8
AmruthaJoseph <124858409+AmruthaJoseph@users.noreply.github.com>
2023-02-23 11:10:43 +0530

    Add files via upload

--- ECommerce_Assessment/src/components/Product.js
+++ ECommerce_Assessment/src/components/Product.js
@@ -8,7 +8,7 @@ function Product() {
                       title:"Brown Egg", 
                       type: "dairy", 
                       description: "Raw organic brown eggs in a basket",
-                      url: "https://via.placeholder.com/150x150.png?text=Product1", 
+                      url: "https://s3.amazonaws.com/images.ecwid.com/images/26273031/1370821191.jpg", 
                       price: 28.1, 
                       rating: 4
                     },
@@ -16,7 +16,7 @@ function Product() {
                       title:"Sweet fresh stawberry", 
                       type: "fruit", 
                       description: "Sweet fresh stawberry on the wooden table",
-                      url: "https://via.placeholder.com/150x150.png?text=Product2",  
+                      url: "https://cgreenag.com/assets/images/crop-images/strawberry/Strawberrieswithleaves..png",
                       price: 29.45, 
                       rating: 4
                     },
@@ -24,16 +24,16 @@ function Product() {
                       title:"Laptop", 
                       type: "electronics", 
                       description: "Dell Latitude 5410",
-                      url: "https://via.placeholder.com/150x150.png?text=Product3",  
+                      url: "https://i5.walmartimages.com/asr/9555580b-d38c-4d19-88b0-61991bf6c33f_1.7fc8d4fa1fa63c94dee13b67aae2e856.jpeg",
                       price: 50000, 
                       rating: 5
                     },
                     {
-                      title:"Apple", 
-                      type: "fruit", 
-                      description: "Red apples",
-                      url: "https://via.placeholder.com/150x150.png?text=Product4",  
-                      price: 50, 
+                      title:"Laptop", 
+                      type: "electronics", 
+                      description: "Lenovo ultra slim thinkpad",
+                      url: "https://allthingsd.com/files/2012/08/U510_hero_04.jpg",
+                      price: 45000, 
                       rating: 5
                     }
                     ];
@@ -56,7 +56,7 @@ function Product() {
             <div className='product_list'>
               <label className='title'> {product.title} </label>
               <br /><br />
-              <img src={product.url} alt="Product" height="150px" width="250px"></img>
+              <img src={product.url} alt="Product" height="250px" width="250px"></img>
               <br />
               <label className='description'> {product.description} </label>
               <br /><br />
